9c987af8122a9d286fb551c858e08117a0efe648
coderheno <vijayarputh@gmail.com>
2025-07-08 13:46:11 +0530

    adding modified files

--- load_model.py
+++ load_model.py
@@ -4,7 +4,7 @@ loaded_models = {
     "Logistic Regression": load('models/logistic_regression_model.joblib'),
     "Decision Tree": load('models/decision_tree_model.joblib'),
     "Random Forest": load('models/random_forest_model.joblib'),
-    "KNN": load('models/naive_bayes_model.joblib'),
+    "KNN": load('models/decision_tree_model.joblib'),
     "Naive Bayes": load('models/naive_bayes_model.joblib'),
     "Gradient Boosting": load('models/gradient_boosting_model.joblib'),
 }
